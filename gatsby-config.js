9ab4c25161b7546caf35c76befbf59693bf7ddb3
Lasse Jantsch <lassejantsch@web.de>
2024-03-21 19:20:09 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `annotation_tool`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
